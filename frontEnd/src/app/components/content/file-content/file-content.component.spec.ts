62a104a57f182c853be34cd48cd3fa27c5f6f27d
mggoaaa <mggoaaa@gmail.com>
2021-12-09 17:35:04 -0600

    Add files via upload

---
+++ frontEnd/src/app/components/content/file-content/file-content.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FileContentComponent } from './file-content.component';
+
+describe('FileContentComponent', () => {
+  let component: FileContentComponent;
+  let fixture: ComponentFixture<FileContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FileContentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FileContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
